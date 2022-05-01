1a9fe2e875a285ff616cfc07bc4737eeeeb7988e
GirardHappy <a.g.ustatutto@gmail.com>
2022-05-01 22:59:07 +0200

    Qualche modifichina

--- app/src/main/java/com/example/prova3/MainActivity.kt
+++ app/src/main/java/com/example/prova3/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.prova3
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    var palina:Int = 968
+    //Metro: 8211 | Mia: 968
+    var palina:Int = 675886
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,30 +20,45 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun btnCerca(v: View) {
+        val t: EditText = findViewById(R.id.txtNum)
         try {
-            Log.d("aaa", "gino")
-            Thread {
-                val url = "https://gtt.to.it/cms/percorari/arrivi?palina=$palina"
-                Scrap(Jsoup.connect(url).get())
-            }.start()
-            Log.d("aaa", "finish")
-        }catch (e:Exception){
-            Log.v("Thread btnCerca", "$e")
-        }
+            palina = t.text.toString().toInt()
+        }catch(e:Exception){return;}
+
+        Thread {
+            try {
+                Scrap(Jsoup.connect("https://gtt.to.it/cms/percorari/arrivi?palina=$palina").get())
+            }catch(e:java.net.UnknownHostException){
+                T("Connessione a Internet Assente")
+            }
+            catch(e:Exception){
+                Log.w("Thread btnCerca", "$e")
+            }
+        }.start()
     }
 
     fun Scrap(d:Document){
-        var t = d.getElementsByClass("bg-warning")[0].getElementsByTag("td")
         var s: String = "";
-        for(i in 1..t.size-1){
-           s+=t[i].text()+"\n"
-        }
-        try {
-            var a: TextView = findViewById(R.id.txb)
-            a.setText(s)
-        }
-        catch (e:Exception){
-            Log.w("Scrap","$e")
-        }
+        val t = d.getElementsByTag("tr")
+            if(t.size>0){
+                for(i in 1..t.size-1) {
+                    for (td in t[i].getElementsByTag("td")){
+                        s += td.text() + " "
+                    }
+                    s += "\n"
+                }
+            }
+            else {
+                s = "Fermata Inesistente"
+            }
+        var a: TextView = findViewById(R.id.txb)
+        if(s.replace("\n","").replace(" ","")=="")
+            s="Nessun passaggio previsto"
+            T(s)
+    }
+
+    fun T(s:String){
+        var a: TextView = findViewById(R.id.txb)
+        a.setText(s)
     }
 }
